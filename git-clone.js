174e22f86ce28ffc45520fcbc64ad422cc461d57
Artur Petrov <artur.petrov@auto.ria.com>
2022-02-22 22:45:08 +0200

    add git pull on dir exist

--- git-clone.js
+++ git-clone.js
@@ -7,12 +7,14 @@ const gitlabCloneOrigin = `${config.web.gitlab.schema}://"${config.git.userName}
 async function main() {
   const hrefs = fs.readFileSync('./hrefs.txt', 'utf-8').split('\n');
   for (const href of hrefs) {
-    try {
-      const gitCommand = `git clone ${gitlabCloneOrigin}${href}.git`;
-      shell.cd('/var/www');
-      shell.exec(gitCommand);
-    } catch (e) {
-      console.error(e.message);
+    const projectPath = '/var/www';
+    const project = href.split('/')[2];
+    if (!fs.existsSync(`${projectPath}/${project}`)) {
+      shell.cd(projectPath);
+      shell.exec(`git clone ${gitlabCloneOrigin}${href}.git`);
+    } else {
+      shell.cd(`${projectPath}/${project}`);
+      shell.exec(`git pull ${gitlabCloneOrigin}${href}.git`);
     }
   }
 }
